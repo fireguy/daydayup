ee160d8e293472b480c7c82e49adc1e374451d90
slfu <slfu@travelsky.com>
2017-09-18 22:52:40 +0800

    helloworld-gradle

---
+++ daydayup/Helloworld-gradle/src/main/java/top/fireguy/helloworld/Helloworld.java
@@ -0,0 +1,7 @@
+package top.fireguy.helloworld;
+
+public class Helloworld{
+		public static void sayHello(String name){
+			System.out.println("Hello, "+ name);
+		}
+}
---
+++ daydayup/Helloworld-gradle/src/test/java/top/fireguy/Helloworld/HelloWorldTest.java
@@ -0,0 +1,13 @@
+package top.fireguy.Helloworld;
+
+import org.junit.Test;
+
+import top.fireguy.helloworld.Helloworld;
+
+public class HelloWorldTest {
+	@Test
+	public void testSayHello() {
+		Helloworld.sayHello("world");
+	}
+
+}
